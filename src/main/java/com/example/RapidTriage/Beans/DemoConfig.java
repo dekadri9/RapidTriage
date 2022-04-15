385973b7fa0f654b24ecf7807ac7b3efd4e0e200
= <=>
2022-04-15 14:14:27 +0200

    Building a DemoUser

---
+++ src/main/java/com/example/RapidTriage/Beans/DemoConfig.java
@@ -0,0 +1,24 @@
+package com.example.RapidTriage.Beans;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import com.example.RapidTriage.Input.UserInput;
+import com.example.RapidTriage.Models.User;
+import com.example.RapidTriage.Repositories.UserRepository;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DemoConfig {
+    User user;
+
+    CommandLineRunner commandLineRunner(UserRepository userRepository){
+        return args -> {
+            UserInput userDetails = new UserInput("demoEmail", "demoName", "demoUsername", "demoPassword", LocalDate.of(1998, 12, 2));
+            user = new User(userDetails.getEmail(), userDetails.getName(), userDetails.getUsername(), userDetails.getPassword(), userDetails.getBirthDate());
+            user.setToken("AUTH");
+        };
+    }
+}
